23ce509faefb7d592296d9885aeddb0f537f90ea
DanielP92Git <pagis.daniel@gmail.com>
2024-02-20 22:05:07 +0200

    Fixed some display issues on main page and added footer section

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,7 @@ import 'regenerator-runtime/runtime';
 import * as categoriesFns from './categories.js';
 import * as homeFn from './homePage.js';
 
+
 // Modal windows:
 //////////////////////////////////////////////////
 // const modal = document.querySelector('.modal');
@@ -40,6 +41,35 @@ import * as homeFn from './homePage.js';
 //----------------------------------------------------
 
 homeFn.checkId();
+
+const menuBars = document.querySelector('.menubars-svg');
+const menuBarsSVG = document.querySelector('.menubars-toggle');
+
+
+const changeSVG = function () {
+  const parent = document.querySelector('.menubars-toggle');
+  parent.classList.toggle('close')
+  const check = parent.classList.contains('close');
+  let icon = '-svg';
+  icon = (!check ? 'close': 'menubars') + icon;
+  document.querySelector("use").setAttribute('href', `#${icon}`);
+
+};
+menuBars.addEventListener('click', changeSVG)
+// const getRect = parent.getClientRects()
+// getRect.addEventListener('scroll', changeSVG);
+
+// const generateMarkup = function () {
+//   const parent = document.querySelector('.menubars-toggle');
+
+//   const markup =
+//   `<use xlink:href="#shoppingcart-svg"></use>`;
+  
+//   parent.remove(markup);
+
+// };
+
+
 //////////////////////////////////////////////////
 // Categories reveal
 
@@ -79,10 +109,10 @@ const menu = document.querySelector('.menu');
 
 const stickyMenu = function (entries) {
   const [entry] = entries;
-
+  
   if (!entry.isIntersecting)
-    menu.classList.add('sticky') + menu.classList.remove('hidden');
-  else menu.classList.remove('sticky');
+  menu.classList.add('sticky') + menu.classList.remove('hidden');
+else menu.classList.remove('sticky');
 };
 
 const headerObserver = new IntersectionObserver(stickyMenu, {
@@ -109,3 +139,21 @@ const headerObserverTwo = new IntersectionObserver(hideMenu, {
 headerObserverTwo.observe(header);
 // Reveal end
 //////////////////////////////////////////////////
+
+
+// const imgs = document.querySelectorAll('.large-img-item');
+// const imgReveal = function(entries, images) {
+  
+//   const [entry] = entries;
+//   const [img] = images;
+//   if (entry.isIntersecting) {
+//     console.log(entries, entros);
+//     img.classList.add('reveal')
+//   }
+// }
+// imgReveal(imgs)
+// const imgObserver = new IntersectionObserver(imgReveal, {
+//   root: null,
+//   threshold: 0,
+// })
+// imgObserver.observe(imgs[0],imgs[1]);
